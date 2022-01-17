c924274575826238b5c107f56a3f8e4c02fd2eba
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-01-17 21:08:52 +0530

    sort

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 const StartBTN = document.getElementById('startBtn');
 const Algorithm = document.getElementById('Algo');
-const ArrayS = document.getElementById('arraySize');
-const ArrayBTN = document.getElementById('makeArray');
 const DisplayBTN = document.getElementById('displayArray');
 const ArrayContainer = document.getElementById('mainTxt');
 const userArray = document.getElementById('array');
@@ -12,10 +10,23 @@ const userNeeds = document.getElementById('searchFor');
 
 let ArraySize = 0;
 let algorithmName = '';
-let array=[];
 let userArrayString;
-let chw;
+let array;
 let newSpan;
+
+let updatedSpan = function() {
+  for (let i = 0; i < ArraySize; i++) {
+    document.getElementById(`span-${i}`).textContent = array[i];
+  }
+}
+
+let sortArr = function(arr){
+  arr.sort();
+  console.log(arr);
+}
+
+  //  functions for search
+      // 1.Linear Search
 let LS = function(z, arr){
     for (let i = 0; i < arr.length; i++) {
       setTimeout(function(){
@@ -34,27 +45,24 @@ let LS = function(z, arr){
     }
 };
 
-//game logic
+// 2.Binary Search
 
-//1. Array
-
-// ArrayBTN.addEventListener('click', function () {
-
-
-//   // 1. making Array of Size : ArraySize
-
-//    array= [ArraySize];
-
- 
-
-  // // 2. entering Elements of array
-  //       for (let i = 0; i < ArraySize; i++) {
-  //         array[i]=Number(prompt(`Enter the ${i} element...`));
-  //         console.log(`at postion ${i} : ${array[i]}`);
-  //       }
-
-
-// });
+let r = ArraySize;
+let l = 0;
+let BS = function (arr,l,r,z) {
+  let mid = l + Math.floor((r-l)/2);
+  if (arr[mid]== z) {
+    console.log(`found at mid`);
+  }
+  if(arr[mid]>z){
+    BS(arr,l,mid-1,z);
+    console.log(`found in left`);
+  }
+  if(arr[mid]<z){
+    BS(arr,mid+1,r,z);
+    console.log(`found in right`);
+  }
+}
 
   // 3. displaying the array 
   DisplayBTN.addEventListener('click', function () {
@@ -63,33 +71,34 @@ let LS = function(z, arr){
 
    //  console.log(userArrayString);
    
-    // chw = comma hatane wala 
-    // const chw = userArrayString.split(',');
+    // array = comma hatane wala 
+    // const array = userArrayString.split(',');
     //comma hatane ke sath string ko number me bdlne waala
 
-    chw = userArrayString.split(',').map(Number);
-    console.log(chw); 
+    array = userArrayString.split(',').map(Number);
+    console.log(array); 
 
 
-    ArraySize =chw.length;
+    ArraySize =array.length;
     console.log(ArraySize);
 
 
 
-    //1.1 making spans of arraySize
-const arrayOfSpans = [];
-for (let i = 0; i < ArraySize; i++) {
- newSpan = document.createElement('span');
- newSpan.id= `span-${i}`;
- newSpan.className= `arraySpan`
- ArrayContainer.appendChild(newSpan);
-//  chw.push(newSpan);
-}
-// ArrayContainer.replaceChildren(...chw);
+//  making spans of arraySize
 
- for (let i = 0; i < ArraySize; i++) {
-   document.getElementById(`span-${i}`).textContent = chw[i];
- }
+        const arrayOfSpans = [];
+        for (let i = 0; i < ArraySize; i++) {
+        newSpan = document.createElement('span');
+        newSpan.id= `span-${i}`;
+        newSpan.className= `arraySpan`
+        //  ArrayContainer.appendChild(newSpan);
+        arrayOfSpans.push(newSpan);
+        }
+        ArrayContainer.replaceChildren(...arrayOfSpans);
+
+// storing array elements into the span
+
+    updatedSpan();
 });
 
 
@@ -100,14 +109,33 @@ StartBTN.addEventListener('click', function(){
   algorithmName = Algorithm.value;
   
   console.log(algorithmName);
-  console.log(chw);
+  console.log(array);
   console.log(userNeeds.value);
   let x = userNeeds.value;
   console.log(ArraySize);
 
   if (algorithmName === 'Linear Search') {
-      LS(x,chw);
+      LS(x,array);
+  } else if(algorithmName === 'Binary Search'){
+
+    //1. call the sortArr function
+        sortArr(array);
+
+    //storing new array to span || updating span values
+        updatedSpan();
+
+    //2. search algorithm.
+        // 2.Binary Search
+
+let r = ArraySize-1;
+let l = 0;
+
+  let mid = l + Math.floor((r-l)/2);
+  if (array[mid]== x) {
+    console.log(`found at mid`);
   }
+}
+  
 });
 
   
